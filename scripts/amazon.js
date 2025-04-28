0a7426c0e796a1ed40d0fb4b1a5748afa31f70f0
MaazCodeCraft <maaz4cs@gmail.com>
2025-04-28 11:29:07 +0500

    Donw with select Tag-> Quantity Update

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach ((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class = "js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -68,12 +68,18 @@ document.querySelectorAll('.js-add-to-cart')
         }
       });
 
+      const quantitySelector = document.querySelector(
+        `.js-quantity-selector-${productId}`
+      );
+
+      const quantity = Number(quantitySelector.value);
+
       if (matchingItems){
-        matchingItems.quantity ++;
+        matchingItems.quantity += quantity;
       } else {
         cart.push ({
           productId,
-          quantity: 1
+          quantity
         });
       }
 
@@ -86,5 +92,8 @@ document.querySelectorAll('.js-add-to-cart')
         .innerHTML = cartQuantity;
       // console.log(cartQuantity);
       // console.log(cart);
+
+      document.querySelector('.js-quantity-selector-${product.id}')
+        
     });
   });
