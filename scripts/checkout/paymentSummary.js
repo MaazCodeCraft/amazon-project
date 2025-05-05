80bdb71e8e4ed8a2fd85a5560a398d5721556d66
MaazCodeCraft <maaz4cs@gmail.com>
2025-05-05 16:08:09 +0500

    Done with Payment Cart Items in Payment Summary

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -20,9 +20,11 @@ export function renderPaymentSummary () {
     const taxCents = totalBeforeTaxCents * 0.1;
     const totalCents = totalBeforeTaxCents + taxCents;
 
-    console.log(totalBeforeTaxCents);
-    console.log(taxCents);
-    console.log(totalCents);
+    let cartQuantity = 0;
+
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+    });
 
     const paymentSummaryHTML = `
         <div class="payment-summary-title">
@@ -30,7 +32,7 @@ export function renderPaymentSummary () {
         </div>
 
           <div class="payment-summary-row">
-            <div>Items (3):</div>
+            <div>Items (${cartQuantity}):</div>
             <div class="payment-summary-money">
                 $${formateCurrency(productPriceCents)}
             </div>
