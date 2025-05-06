c1e67b475adb19a86a118161b2bc5a971c7348c4
MaazCodeCraft <maaz4cs@gmail.com>
2025-05-06 18:26:07 +0500

    Create automated tests for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,29 @@
+import { formateCurrency } from "../scripts/Utils/money.js";
+
+console.log("test suite: formate currency");
+
+//Basic Test Case
+console.log("convert cents into dollors");
+
+if (formateCurrency (2095) === '20.95'){
+    console.log("passed");
+} else {
+    console.log("failed");
+}
+
+//Edge Test Case
+console.log("work with 0");
+
+if (formateCurrency (0) === '0.00'){
+    console.log("passed");
+} else {
+    console.log("failed");
+}
+
+console.log("rounds up to the nearest cents");
+
+if (formateCurrency (2000.5) === '20.01'){
+    console.log("passed");
+} else {
+    console.log("failed");
+}
