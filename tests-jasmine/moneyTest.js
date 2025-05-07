84ecc42d9136ec9a4305424a2ecc40c8126ba46a
MaazCodeCraft <maaz4cs@gmail.com>
2025-05-07 16:57:01 +0500

    remove default test and add money tests

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,18 @@
+import { formateCurrency } from "../scripts/Utils/money.js";
+
+describe("test suite: formate currency", () => {
+    describe("rounding", () => {
+        it("convert cents into dollors", () => {
+            expect(formateCurrency (2095)).toEqual('20.95');
+        });
+    
+        it("work with 0", () => {
+            expect(formateCurrency (0)).toEqual('0.00');
+        });
+    
+        it("rounds up to the nearest cents", () => {
+            expect(formateCurrency (2000.5)).toEqual('20.01');
+        });
+    });
+    
+});
