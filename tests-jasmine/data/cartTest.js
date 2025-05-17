728637f35fcd6c845602a84c2bc0b3bad44e8547
MaazCodeCraft <maaz4cs@gmail.com>
2025-05-17 17:41:26 +0500

    Create automated test for addToCart

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -1,5 +1,21 @@
-import { addToCart } from "../../data/cart.js";
+import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
 
 describe ("test suite: add to cart", () => {
-    it("")
+    it("add an existing product to the cart", () => {
+        
+    });
+
+    it("adds a new product to the cart", () => {
+
+        spyOn (localStorage, 'setItem');
+
+        spyOn (localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([]);
+        });
+        loadFromStorage();
+        
+        addToCart ('e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 5);
+        except (cart.length).toEqual(1);
+        except (localStorage.setItem).toHaveBeenCalledTimes(1);
+    });
 });
