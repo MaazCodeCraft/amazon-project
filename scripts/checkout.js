23b6b649ccf9f722f54e29b7fcb2c54cefa58ea1
MaazCodeCraft <maaz4cs@gmail.com>
2025-06-04 16:48:15 +0500

    Create order using the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,7 @@ import { renderOrderSummary  } from "./checkout/orderSumary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/cart-class.js'
 // import '../data/car.js';
-import '../data/backend-practice.js';
+// import '../data/backend-practice.js';
 
 
 renderOrderSummary ();
