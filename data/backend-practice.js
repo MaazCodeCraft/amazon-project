12cbb033925ed401e8b47c63bfc545c69ab32383
MaazCodeCraft <maaz4cs@gmail.com>
2025-06-04 15:32:37 +0500

    Learn fetch

--- data/backend-practice.js
+++ data/backend-practice.js
@@ -6,3 +6,16 @@ xhr.addEventListener ('load', () => {
 
 xhr.open('GET', 'https://supersimplebackend.dev');
 xhr.send();
+
+// Simple than XMLHttpRequest 
+// Fetch Uses Promises
+function fetchLearn () {
+    fetch('https://supersimplebackend.dev/products').then((response) => {
+        // console.log(response);
+        return response.json(); // json is also asyncronous it return a promise
+    }).then ((data) => {
+        console.log(data);
+    });
+}
+
+fetchLearn();
