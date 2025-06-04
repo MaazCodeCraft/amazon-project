494f59debe76372c269a9259ec3f8447f9096937
MaazCodeCraft <maaz4cs@gmail.com>
2025-06-04 15:21:14 +0500

    Learn Promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,5 +4,37 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/car.js';
 import '../data/backend-practice.js';
 
+
 renderOrderSummary ();
 renderPaymentSummary();
+
+Promise.all([
+    new Promise ((resolve) => {
+        console.log('Promise');
+        resolve('value1');
+    }),
+    new Promise((resolve) => {
+        console.log('Promise2');
+        resolve();
+    })
+]).then ((values) => {
+    console.log(values);
+    console.log('Next Step!');
+});
+
+/*
+new Promise ((resolve) => {
+    console.log('Promise');
+    resolve('value1');
+
+}).then ((value) => {
+    console.log(value);
+    return new Promise((resolve) => {
+        console.log('Promise2');
+        resolve();
+    })
+
+}).then (() => {
+    console.log('Next Step!');
+});
+*/
